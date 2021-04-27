5d45cd3fd0db99f517dcd3ce050e8d066372bc38
k3v1nn90 <77991437+k3v1nn90@users.noreply.github.com>
2021-04-27 08:01:22 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -15,21 +15,21 @@ red = redis.Redis(host="redis-server", db=0, socket_connect_timeout=2, socket_ti
 
 app = Flask(__name__)
 
-@app.route("/md5/<string:words>")
-def md5(words, chars=string.ascii_letters + string.digits):
-    x="{"
-    y="}"
-    leng = len(words)
-    words = urllib.parse.quote(words)
-    txt=(''.join(random.choice(chars) for x in range(leng)))
-    return f"{x}\n\"input\": {words},\n\"output\": {txt}\n{y}"
+# @app.route("/md5/<string:words>")
+# def md5(words, chars=string.ascii_letters + string.digits):
+#     x="{"
+#     y="}"
+#     leng = len(words)
+#     words = urllib.parse.quote(words)
+#     txt=(''.join(random.choice(chars) for x in range(leng)))
+#     return f"{x}\n\"input\": {words},\n\"output\": {txt}\n{y}"
 
-# @app.route('/md5/<string:words>')
-# def md5(words):
-#     return jsonify(
-#         input=words,
-#         output=hashlib.md5(words).hexdigest()
-#     )
+@app.route('/md5/<string:words>')
+def md5(words):
+    return jsonify(
+        input=words,
+        output=hashlib.md5(words.encode('utf8')).hexdigest()
+    )
 
 @app.route("/factorial/<int:num>")
 def factor(num,fact=1):
